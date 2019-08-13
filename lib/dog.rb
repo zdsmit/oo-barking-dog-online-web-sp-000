47c2f0b0a756b0ffdfdd565b4a5c2799d5126f25
Zach  Smith <zdsmith001@gmail.com>
2019-08-13 22:31:08 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Dog
+  def name=(dogs_name)
+    @the_dogs_name = dogs_name
+  end
+
+  def name
+    @the_dogs_name
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
