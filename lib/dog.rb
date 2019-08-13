31febb517dcead72ac942a6ffb0aa7949fc54895
Zach  Smith <zdsmith001@gmail.com>
2019-08-13 22:29:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 # Your code goes here!
 class Dog
   def name=(dogs_name)
-    the_dogs_name = dogs_name
+    @the_dogs_name = dogs_name
   end
 
   def name
-    the_dogs_name
+    @the_dogs_name
   end
 end
